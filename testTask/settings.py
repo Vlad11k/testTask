f80adfc3467a8123e202d1e853c3dc373b6b60c4
Vlad Prokopov <vladprokopov2509@gmail.com>
2024-06-26 01:44:03 +0300

    Create User model

--- testTask/settings.py
+++ testTask/settings.py
@@ -12,6 +12,8 @@ DEBUG = bool(os.getenv('DEBUG', default=1))
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(' ')
 
+AUTH_USER_MODEL = "users.User"
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- users/models.py
+++ users/models.py
@@ -1,5 +1,67 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, UserManager
+from django.db import models
 
 
-class User(AbstractUser):
-    pass
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    # first_name = models.CharField(verbose_name='first name', max_length=150, blank=True)
+    # last_name = models.CharField(verbose_name='last name', max_length=150, blank=True)
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+        blank=True
+    )
+    phone = models.CharField(
+        verbose_name='phone',
+        min_length=9,
+        max_length=100,
+        unique=True,
+        blank=True)
+    full_name = models.CharField(
+        verbose_name='first name',
+        max_length=150,
+        blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
