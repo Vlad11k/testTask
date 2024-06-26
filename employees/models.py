0f56871725b1078f746bef61679f935c73fa8a2e
Vlad Prokopov <vladprokopov2509@gmail.com>
2024-06-26 04:53:53 +0300

    Employees api

--- employees/models.py
+++ employees/models.py
@@ -1,22 +1,34 @@
 from django.db import models
 
-from users.models import User
-
 
 class Employee(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE
+
+    full_name = models.CharField(
+        verbose_name='ФИО',
+        max_length=150,
+        blank=True)
+    email = models.EmailField(
+        verbose_name="Email",
+        max_length=255,
+        unique=True,
+        blank=True
+    )
+    phone = models.CharField(
+        verbose_name='Телефон',
+        max_length=100,
+        unique=True,
+        blank=True
     )
     photo = models.ImageField(
         upload_to="photos/%Y/%m/%d/",
         default=None,
         blank=True,
         null=True,
-        verbose_name="Фото")
+        verbose_name="Фото"
+    )
 
     def __str__(self):
-        return self.user.email
+        return self.full_name
 
     class Meta:
         verbose_name = "Сотрудник"
