ec5f4758ed76a6bb5a12a1c4177dbb10f66d15bc
Vlad Prokopov <vladprokopov2509@gmail.com>
2024-06-28 01:18:10 +0300

    Update models

--- tasks/models.py
+++ tasks/models.py
@@ -30,7 +30,7 @@ class Task(models.Model):
     date_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     date_update = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
     date_end = models.DateTimeField(blank=True, null=True, verbose_name='Дата закрытия')
-    report = models.TextField(default=None, verbose_name='Отчёт')
+    report = models.TextField(default=None, null=True, verbose_name='Отчёт')
     status = models.CharField(
         choices=STATUS_CHOICES,
         default=WAITING,
