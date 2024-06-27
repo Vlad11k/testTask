a1f8d06c8a65c83dfc3ef4e7b494ea57f2739256
Vlad Prokopov <vladprokopov2509@gmail.com>
2024-06-27 05:22:58 +0300

    Client api permissions

--- clients/views.py
+++ clients/views.py
@@ -1,19 +1,28 @@
-from django.shortcuts import render
-from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from clients.models import Client
+from clients.permissions import IsOwner, IsEmployee
 from clients.serializers import ClientSerializer
 
 
-class ClientViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    GenericViewSet,
-):
-
+class ClientAPIList(generics.ListCreateAPIView):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    permission_classes = (IsEmployee, )
 
 
+class ClientAPIRetrieve(generics.RetrieveAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = (IsEmployee, )
+
+
+class ClientAPIProfile(generics.ListAPIView):
+    serializer_class = ClientSerializer
+
+    def get_queryset(self):
+        client = self.request.user
+        if client.is_authenticated:
+            return Client.objects.filter(user_ptr=client)
+        return self.permission_denied(self.request)
--- testTask/urls.py
+++ testTask/urls.py
@@ -3,16 +3,19 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from clients.views import ClientViewSet
+# from clients.views import ClientViewSet
+from clients.views import ClientAPIList, ClientAPIRetrieve, ClientAPIProfile
 from employees.views import EmployeeViewSet
 
 router = DefaultRouter()
-router.register(r"client", ClientViewSet, basename='client')
 router.register(r"employee", EmployeeViewSet, basename='employee')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
+    path('api/v1/client/', ClientAPIList.as_view()),
+    path('api/v1/client/<int:pk>/', ClientAPIRetrieve.as_view()),
+    path('api/v1/client/profile', ClientAPIProfile.as_view()),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
