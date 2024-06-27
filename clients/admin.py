4f256e7091c68c1b91cf7da01005c679fb8b4994
Vlad Prokopov <vladprokopov2509@gmail.com>
2024-06-27 09:25:44 +0300

    Fix admin page

--- clients/admin.py
+++ clients/admin.py
@@ -5,17 +5,10 @@ from clients.models import Client
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('get_full_name', 'get_email', 'get_phone')
+    list_display = ('full_name', 'email', 'phone')
 
-    @admin.display(description='ФИО')
-    def get_full_name(self, obj):
-        return f"{obj.user.full_name}"
-
-    @admin.display(description='Email')
-    def get_email(self, obj):
-        return f"{obj.user.email}"
-
-    @admin.display(description='Телефон')
-    def get_phone(self, obj):
-        return f"{obj.user.phone}"
+    fieldsets = [
+        (None, {"fields": ["email"]}),
+        ("Личная информация", {"fields": ["full_name", "phone"]}),
+    ]
 
--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from employees.models import Employee
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'phone')
+
+    fieldsets = [
+        (None, {"fields": ["email"]}),
+        ("Личная информация", {"fields": ["full_name", "phone"]}),
+        ("Права", {"fields": ["is_staff"]}),
+    ]
